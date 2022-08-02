7417f6b8006a7111b7b42c3814d872d0c193c55c
Rocky Mandayam <rockymandayam@fb.com>
2022-08-01 17:59:24 -0700

    Add Constants.java which I forgot to add in an earlier commit

---
+++ src/main/java/com/facebook/openwifirrm/optimizers/Constants.java
@@ -0,0 +1,10 @@
+package com.facebook.openwifirrm.optimizers;
+
+public final class Constants {
+
+	/** String of the 2.4 GHz band */
+	public static final String BAND_2G = "2G";
+	/** String of the 5 GHz band */
+	public static final String BAND_5G = "5G";
+
+}
