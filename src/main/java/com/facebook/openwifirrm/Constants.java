daa9fba6b02789dfbbb9cc0bc63fc2248d784c63
Rocky Mandayam <rockymandayam@fb.com>
2022-08-03 23:37:52 -0700

    Address some PR comments.

--- src/main/java/com/facebook/openwifirrm/Constants.java
+++ src/main/java/com/facebook/openwifirrm/Constants.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package com.facebook.openwifirrm;
 
 public final class Constants {
@@ -6,7 +14,7 @@ public final class Constants {
 	public static final String BAND_2G = "2G";
 	/** String of the 5 GHz band */
 	public static final String BAND_5G = "5G";
-	
+
 	// This class should not be instantiated.
 	private Constants() {}
 
