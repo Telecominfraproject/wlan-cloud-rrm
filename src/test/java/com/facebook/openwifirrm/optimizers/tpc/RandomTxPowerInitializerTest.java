c06bb822eadd9ccc5556d442750e613f7a0a6e59
Wuwei Cai <wuweicai@fb.com>
2022-08-31 15:36:56 -0700

    Use availableTxPowersList in random TPC
    Signed-off-by: Wuwei Cai <wuweicai@fb.com>

--- src/test/java/com/facebook/openwifirrm/optimizers/tpc/RandomTxPowerInitializerTest.java
+++ src/test/java/com/facebook/openwifirrm/optimizers/tpc/RandomTxPowerInitializerTest.java
@@ -8,6 +8,8 @@
 
 package com.facebook.openwifirrm.optimizers.tpc;
 
+import com.facebook.openwifirrm.DeviceConfig;
+import com.facebook.openwifirrm.DeviceLayeredConfig;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -19,13 +21,13 @@ import com.facebook.openwifirrm.optimizers.TestUtils;
 import com.facebook.openwifirrm.ucentral.UCentralConstants;
 import com.facebook.openwifirrm.ucentral.models.State;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestMethodOrder(OrderAnnotation.class)
 public class RandomTxPowerInitializerTest {
@@ -96,25 +98,46 @@ public class RandomTxPowerInitializerTest {
 	@Test
 	@Order(2)
 	void testRandomTxPowerInAvailableList() throws Exception {
+		DeviceDataManager deviceDataManager = createDeviceDataManager();
+		DeviceLayeredConfig deviceLayeredConfig = new DeviceLayeredConfig();
+		DeviceConfig deviceConfigA = new DeviceConfig();
+		deviceConfigA.allowedTxPowers = new TreeMap<>();
+		deviceConfigA.allowedTxPowers
+			.put(UCentralConstants.BAND_2G, Arrays.asList(11, 12, 13, 14, 15));
+		deviceConfigA.userTxPowers = new TreeMap<>();
+		deviceConfigA.userTxPowers.put(UCentralConstants.BAND_2G, 6);
+		deviceConfigA.allowedTxPowers
+			.put(UCentralConstants.BAND_5G, Arrays.asList(11, 12, 13, 14, 15));
+		deviceLayeredConfig.apConfig.put(DEVICE_A, deviceConfigA);
+		DeviceConfig deviceConfigB = new DeviceConfig();
+		deviceConfigB.userTxPowers = new TreeMap<>();
+		deviceConfigB.userTxPowers.put(UCentralConstants.BAND_2G, 7);
+		deviceLayeredConfig.apConfig.put(DEVICE_B, deviceConfigB);
+		deviceDataManager.setDeviceLayeredConfig(deviceLayeredConfig);
 		TPC optimizer = new RandomTxPowerInitializer(
 			createModel(),
 			TEST_ZONE,
-			createDeviceDataManager(),
+			deviceDataManager,
 			new Random(0)
 		);
 		Map<String, Map<String, Integer>> txPowerMap =
 			optimizer.computeTxPowerMap();
-		Set<Integer> txPowerSet = new TreeSet<>();
-		for (String serialNumber : txPowerMap.keySet()) {
-			txPowerSet.addAll(txPowerMap.get(serialNumber).values());
-		}
 
-		// different values per AP
-		assertTrue(txPowerSet.size() > 1);
-		for (int txPower : txPowerSet) {
-			assertTrue(
-				txPower >= TPC.MIN_TX_POWER && txPower <= TPC.MAX_TX_POWER
-			);
-		}
+		assertEquals(
+			txPowerMap.get(DEVICE_A).get(UCentralConstants.BAND_2G),
+			6
+		);
+		assertEquals(
+			txPowerMap.get(DEVICE_A).get(UCentralConstants.BAND_5G),
+			13
+		);
+		assertEquals(
+			txPowerMap.get(DEVICE_B).get(UCentralConstants.BAND_2G),
+			7
+		);
+		assertEquals(
+			txPowerMap.get(DEVICE_B).get(UCentralConstants.BAND_5G),
+			2
+		);
 	}
 }
