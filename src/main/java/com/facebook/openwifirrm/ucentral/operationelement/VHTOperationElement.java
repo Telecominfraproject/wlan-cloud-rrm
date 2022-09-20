c6d828944fe67d41f3a67841e4bc5489dec93a17
Rocky Mandayam <rockymandayam@fb.com>
2022-09-20 14:41:10 -0700

    Use int instead of byte to store unsigned byte values
    Signed-off-by: Rocky Mandayam <rockymandayam@fb.com>

--- src/main/java/com/facebook/openwifirrm/ucentral/operationelement/VHTOperationElement.java
+++ src/main/java/com/facebook/openwifirrm/ucentral/operationelement/VHTOperationElement.java
@@ -30,15 +30,23 @@ public class VHTOperationElement {
 	 * 160 MHz wide channel, this parameter is the channel number of the 80MHz
 	 * channel that contains the primary channel. For a 80+80 MHz wide channel, this
 	 * parameter is the channel number of the primary channel.
+	 * <p>
+	 * This field is an unsigned byte in the specification (i.e., with values
+	 * between 0 and 255). But because Java only supports signed bytes, an int
+	 * data type is used to store the value.
 	 */
-	public final byte channel1;
+	public final int channel1;
 	/**
 	 * This should be zero unless the channel is 160MHz or 80+80 MHz wide. If the
 	 * channel is 160 MHz wide, this parameter is the channel number of the 160 MHz
 	 * wide channel. If the channel is 80+80 MHz wide, this parameter is the channel
 	 * index of the secondary 80 MHz wide channel.
+	 * <p>
+	 * This field is an unsigned byte in the specification (i.e., with values
+	 * between 0 and 255). But because Java only supports signed bytes, an int
+	 * data type is used to store the value.
 	 */
-	public final byte channel2;
+	public final int channel2;
 	/**
 	 * An 8-element array where each element is between 0 and 4 inclusive. MCS means
 	 * Modulation and Coding Scheme. NSS means Number of Spatial Streams. There can
@@ -60,8 +68,8 @@ public class VHTOperationElement {
 	public VHTOperationElement(String vhtOper) {
 		byte[] bytes = Base64.decodeBase64(vhtOper);
 		this.channelWidth = bytes[0];
-		this.channel1 = bytes[1];
-		this.channel2 = bytes[2];
+		this.channel1 = bytes[1] & 0xff; // read as unsigned value
+		this.channel2 = bytes[2] & 0xff; // read as unsigned value
 		byte[] vhtMcsForNss = new byte[8];
 		vhtMcsForNss[0] = (byte) (bytes[3] >>> 6);
 		vhtMcsForNss[1] = (byte) ((bytes[3] & 0b00110000) >>> 4);
@@ -83,8 +91,8 @@ public class VHTOperationElement {
 	 */
 	public VHTOperationElement(
 		byte channelWidth,
-		byte channel1,
-		byte channel2,
+		int channel1,
+		int channel2,
 		byte[] vhtMcsForNss
 	) {
 		/*
