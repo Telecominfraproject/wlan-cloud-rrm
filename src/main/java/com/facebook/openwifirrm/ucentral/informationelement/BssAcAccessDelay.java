4b4936438f1cf2942e0aa90d064283f09635f3ef
Jun Woo Shin <jwoos@fb.com>
2022-10-05 12:57:36 -0400

    update BssAcAccessDelay
    Signed-off-by: Jun Woo Shin <jwoos@fb.com>

--- src/main/java/com/facebook/openwifirrm/ucentral/informationelement/BssAcAccessDelay.java
+++ src/main/java/com/facebook/openwifirrm/ucentral/informationelement/BssAcAccessDelay.java
@@ -22,10 +22,112 @@ public class BssAcAccessDelay {
 	/** Defined in 802.11 table 9-92 */
 	public static final int TYPE = 68;
 
-	public final int accessCategoryAccessDelay;
+	/**
+	 * Subfield that goes into Access Category Access Delay field in BSS AC
+	 * Access Delay. For information on what the values mean, check section
+	 * 9.4.2.43
+	 */
+	public static class AccessCategoryAccessDelay {
+		/**
+		 * Unsigned int that represents a scaled representation of best effort AC
+		 * access delay
+		 */
+		public final short averageAccessDelayForBestEffort;
+		/**
+		 * Unsigned int that represents a scaled representation of background AC
+		 * access delay
+		 */
+		public final short averageAccessDelayForBackground;
+		/**
+		 * Unsigned int that represents a scaled representation of video AC access
+		 * delay
+		 */
+		public final short averageAccessDelayForVideo;
+		/**
+		 * Unsigned int that represents a scaled representation of voice AC access
+		 * delay
+		 */
+		public final short averageAccessDelayForVoice;
+
+		/** Constructor */
+		public AccessCategoryAccessDelay(
+			short averageAccessDelayForBestEffort,
+			short averageAccessDelayForBackground,
+			short averageAccessDelayForVideo,
+			short averageAccessDelayForVoice
+		) {
+			this.averageAccessDelayForBestEffort =
+				averageAccessDelayForBestEffort;
+			this.averageAccessDelayForBackground =
+				averageAccessDelayForBackground;
+			this.averageAccessDelayForVideo = averageAccessDelayForVideo;
+			this.averageAccessDelayForVoice = averageAccessDelayForVoice;
+		}
+
+		/** Parse AccessCategoryAccessDelay from JSON object */
+		// TODO rename fields as necessary - we don't know how the data format yet
+		public static AccessCategoryAccessDelay parse(JsonObject contents) {
+			return new AccessCategoryAccessDelay(
+				contents.get("Average Access Delay For Best Effort").getAsShort(),
+				contents.get("Average Access Delay For Background").getAsShort(),
+				contents.get("Average Access Delay For Video").getAsShort(),
+				contents.get("Average Access Delay For Voice").getAsShort()
+			);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(
+				averageAccessDelayForBestEffort,
+				averageAccessDelayForBestEffort,
+				averageAccessDelayForVideo,
+				averageAccessDelayForVoice
+			);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null) {
+				return false;
+			}
+
+			if (this == obj) {
+				return true;
+			}
+
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+
+			AccessCategoryAccessDelay other = (AccessCategoryAccessDelay) obj;
+			return averageAccessDelayForBestEffort ==
+				other.averageAccessDelayForBestEffort &&
+				averageAccessDelayForBackground ==
+					other.averageAccessDelayForBackground &&
+				averageAccessDelayForVideo ==
+					other.averageAccessDelayForVideo &&
+				averageAccessDelayForVoice == other.averageAccessDelayForVoice;
+		}
+
+		@Override
+		public String toString() {
+			return String.format(
+				"AccessCategoryAccessDelay[averageAccessDelayForBestEffort=%d, averageAccessDelayForBackground=%d, averageAccessDelayForVideo=%d, averageAccessDelayForVoice=%d]",
+				averageAccessDelayForBestEffort,
+				averageAccessDelayForBackground,
+				averageAccessDelayForVideo,
+				averageAccessDelayForVoice
+			);
+		}
+	}
+
+	/** Holds AccessCategoryAccessDelay subfield */
+	public final AccessCategoryAccessDelay accessCategoryAccessDelay;
 
 	/** Constructor */
-	public BssAcAccessDelay(int accessCategoryAccessDelay) {
+	public BssAcAccessDelay(
+		AccessCategoryAccessDelay accessCategoryAccessDelay
+	) {
 		this.accessCategoryAccessDelay = accessCategoryAccessDelay;
 	}
 
@@ -33,7 +135,9 @@ public class BssAcAccessDelay {
 	// TODO rename fields as necessary - we don't know how the data format yet
 	public static BssAcAccessDelay parse(JsonObject contents) {
 		return new BssAcAccessDelay(
-			contents.get("AP Average Access Delay").getAsByte()
+			AccessCategoryAccessDelay.parse(
+				contents.get("AP Average Access Delay").getAsJsonObject()
+			)
 		);
 	}
 
@@ -63,7 +167,7 @@ public class BssAcAccessDelay {
 	@Override
 	public String toString() {
 		return String.format(
-			"BssAcAccessDelay[accessCategoryAccessDelay=%d]",
+			"BssAcAccessDelay[accessCategoryAccessDelay=%s]",
 			accessCategoryAccessDelay
 		);
 	}
