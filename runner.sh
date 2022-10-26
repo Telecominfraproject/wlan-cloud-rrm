1638f2cbcb585f2db8a90841c14dfcea1ccebc5b
Jun Woo Shin <jwoos@meta.com>
2022-10-26 16:57:59 -0400

    Extend runner.sh to allow both hotspot and openj9 jvm
    Signed-off-by: Jun Woo Shin <jwoos@meta.com>

--- runner.sh
+++ runner.sh
@@ -1,11 +1,29 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-BIN=$1
-shift 1
+# script to help run the binary with specific jvm and jvm options
+# Defaults to openj9. Turn on hotspot by setting env JVM_HOTSPOT
 
-echo $@
+JVM=$1
+BIN=$2
+shift 2
 
-java \
-	-XX:+IdleTuningGcOnIdle -Xtune:virtualized \
+COMMON_PARAMETERS=()
+
+if [ -n "$JVM_HOTSPOT" ]; then
+# for hotspot
+PARAMETERS=(
+"${COMMON_PARAMETERS[@]}"
+)
+else
+# for openj9
+PARAMETERS=(
+"${COMMON_PARAMETERS[@]}"
+-XX:+IdleTuningGcOnIdle
+-Xtune:virtualized
+)
+fi
+
+"$JVM" \
+	"${PARAMETERS[@]}" \
 	-jar "$BIN" \
 	"$@"
