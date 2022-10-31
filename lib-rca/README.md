14ef67cd8f41e1682d8f59b5c5437967ed6bf462
Rocky Mandayam <rockymandayam@fb.com>
2022-10-31 15:37:19 -0700

    Address comments
    Signed-off-by: Rocky Mandayam <rockymandayam@fb.com>

--- lib-rca/README.md
+++ lib-rca/README.md
@@ -1,2 +1,3 @@
 # Root Cause Analysis (RCA) Java Library
 A Java library which analyzes statistics and provides root cause analysis (RCA) for clients.
+This is a work in progress.
--- lib-rca/src/main/java/com/facebook/openwifi/librca/RootCauseAnalyzer.java
+++ lib-rca/src/main/java/com/facebook/openwifi/librca/RootCauseAnalyzer.java
@@ -10,8 +10,4 @@ package com.facebook.openwifi.librca;
 
 public class RootCauseAnalyzer {
 
-	/** Main method. Placeholder code. */
-	public static void main(String[] args) {
-		System.out.println("Placeholder.");
-	}
 }
