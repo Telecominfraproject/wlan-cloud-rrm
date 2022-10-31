a8472f22a948faddf63fb0d9c4195f3be85c8d75
Rocky Mandayam <rockymandayam@fb.com>
2022-10-31 09:11:47 -0700

    Revert "Add lib-rca sub-module"
    This reverts commit f1cfea4a9ff1c8fcd80f2afc1bc8142f9e70aeb2.

--- lib-rca/README.md
+++
@@ -1,2 +0,0 @@
-# OpenWiFi Root Cause Analysis (RCA) Java Library
-A Java library which analyzes statistics and provides root cause analysis (RCA) for clients.
--- lib-rca/src/main/java/com/facebook/openwifi/librca/RootCauseAnalyzer.java
+++
@@ -1,9 +0,0 @@
-package com.facebook.openwifi.librca;
-
-public class RootCauseAnalyzer {
-
-	/** Main method. Placeholder code. */
-	public static void main(String[] args) {
-		System.out.println("Placeholder.");
-	}
-}
