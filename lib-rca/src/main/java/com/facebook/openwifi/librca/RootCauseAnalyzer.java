f1cfea4a9ff1c8fcd80f2afc1bc8142f9e70aeb2
Rocky Mandayam <rockymandayam@fb.com>
2022-10-31 09:10:30 -0700

    Add lib-rca sub-module
    Signed-off-by: Rocky Mandayam <rockymandayam@fb.com>

---
+++ lib-rca/src/main/java/com/facebook/openwifi/librca/RootCauseAnalyzer.java
@@ -0,0 +1,9 @@
+package com.facebook.openwifi.librca;
+
+public class RootCauseAnalyzer {
+
+	/** Main method. Placeholder code. */
+	public static void main(String[] args) {
+		System.out.println("Placeholder.");
+	}
+}
